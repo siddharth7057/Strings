3e0395b9e355d76129525039a5a7f5cf70156d5a
siddharth7057 <jadhavsiddharth014@gmail.com>
2026-01-09 15:55:34 +0530

    String class constructors

---
+++ StringClassConstructor.java
@@ -0,0 +1,26 @@
+public class StringClassConstructor
+{
+	//string class constructors.
+	
+	//static String s;
+	//static String s1 = new String();
+	//static String s2 = new String("good");
+	public static void main(String x[])
+	{
+		//System.out.println("Value of s is : "+s);
+		//System.out.println("Value of s1 is : "+s1);
+		//System.out.println("Value of s2 is : "+s2);
+
+		//char ch[] = new char[]{'a','b','c','d','e','f'};
+		//String s3 = new String(ch);
+		//System.out.println(s3);
+
+		//byte b[] = new byte[]{97,98,99,100,101};
+		//String s4 = new String(b);
+		//System.out.println(s4);
+
+		//char ch1[] = new char[]{'g','o','o','d',' ','m','o','r','n','i','n','g',' ','i','n','d','i','a'};
+		//String s5  = new String(ch1,5,7);
+		//System.out.println(s5);
+	}	
+}
